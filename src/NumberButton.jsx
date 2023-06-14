cb592fd1a1cb5ee6dc85204b413687f0dd9b8351
thomas-spinec <thomas.spinec@laplateforme.io>
2023-06-14 16:00:17 +0200

    petit changement affichage des numéros

--- src/NumberButton.jsx
+++ src/NumberButton.jsx
@@ -1,7 +1,7 @@
 import Button from "./Button.jsx";
 
 export default function NumberButton() {
-    const numbers = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+    const numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
     const listButton = numbers.map((number, id) => {
         return <Button value={number} key={id}/>
     });
